ebc25dcd7fdacdb4109969e4770ecaaaa091b056
vineethdk <vineethdk31@gmail.com>
2020-11-15 14:48:37 +0530

    Added func. of Updating the notice

--- app.js
+++ app.js
@@ -17,123 +17,128 @@ const saltRounds = 10;
 //Connecting to real Database in mongoose website.
 //mongoose.connect('mongodb+srv://admin-vineeth:Test123@cluster0.rbcaz.mongodb.net/<dbname>', {useNewUrlParser: true, useUnifiedTopology: true});
 //Connecting to local db in our computer.
-mongoose.connect('mongodb://localhost:27017/imaDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/imaDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
- app.use(bodyParser.urlencoded({extended:true}));//To grab the form that is entered by user.
- app.use(bodyParser.json())
-app.use(express.static("public"));//folders like css and images and sounds etc.
-app.set('view engine', 'ejs');//install ejs also.
+app.use(bodyParser.urlencoded({
+  extended: true
+})); //To grab the form that is entered by user.
+app.use(bodyParser.json())
+app.use(express.static("public")); //folders like css and images and sounds etc.
+app.set('view engine', 'ejs'); //install ejs also.
 
 var imageSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    dept:String,
-    date:Date,
-    img:{
-        data: Buffer,
-        contentType: String
-    },
-    ques:[],
-    answ:[]
+  name: String,
+  desc: String,
+  dept: String,
+  date: Date,
+  img: {
+    data: Buffer,
+    contentType: String
+  },
+  ques: [],
+  answ: []
 });
 
-const imgModel = mongoose.model("Image",imageSchema);
+const imgModel = mongoose.model("Image", imageSchema);
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 });
 
 
-var upload = multer({ storage: storage });
+var upload = multer({
+  storage: storage
+});
 
 
 
 //**************************************************************************Tesing**********************************************//
 
 const userSchema = new mongoose.Schema({
-  fname:String,
-  lname:String,
-  uname:String,
-  staffid:String,
-  dept:String,
-  email:String,
-  password:String
+  fname: String,
+  lname: String,
+  uname: String,
+  staffid: String,
+  dept: String,
+  email: String,
+  password: String
 });
 
-const User = new mongoose.model("User",userSchema)
+const User = new mongoose.model("User", userSchema)
 
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home")
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
   res.render("login")
 });
 
-app.post("/login",function(req,res){
-  User.findOne({email:req.body.username},function(err,foundOne){
-      if(foundOne){
-        bcrypt.compare(req.body.password, foundOne.password, function(err, result) {
-          if(result===true){
-        res.redirect('/wishes?valid='+ foundOne._id)
-      }
-      else{
-        res.status(404).send("USERNAME OR PASSWORD IS WORNG CHECK ONCE AGAIN");
-      }
+app.post("/login", function(req, res) {
+  User.findOne({
+    email: req.body.username
+  }, function(err, foundOne) {
+    if (foundOne) {
+      bcrypt.compare(req.body.password, foundOne.password, function(err, result) {
+        if (result === true) {
+          res.redirect('/wishes?valid=' + foundOne._id)
+        } else {
+          res.status(404).send("USERNAME OR PASSWORD IS WORNG CHECK ONCE AGAIN");
+        }
       });
-      }
-      else{
-          console.log("Not yet registered");
-      }
-    })
+    } else {
+      console.log("Not yet registered");
+    }
+  })
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
   res.render("register")
 });
 
-app.post("/register",function(req,res){
-   //USER - STAFF //REMEMBER//
+app.post("/register", function(req, res) {
+  //USER - STAFF //REMEMBER//
   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User(
-      {
-        fname:req.body.fname,
-        lname:req.body.lname,
-        uname:req.body.uname,
-        staffid:req.body.sid,
-        dept:req.body.dname,
-        email:req.body.username,
-        password:hash
-      }
-    );
+    const newUser = new User({
+      fname: req.body.fname,
+      lname: req.body.lname,
+      uname: req.body.uname,
+      staffid: req.body.sid,
+      dept: req.body.dname,
+      email: req.body.username,
+      password: hash
+    });
 
-    User.find({email:req.body.username},function(err,foundItem){
-      if(!err){
+    User.find({
+      email: req.body.username
+    }, function(err, foundItem) {
+      if (!err) {
         console.log(foundItem)
-        if(foundItem.length===0){
-          newUser.save(function(err){
-            if(!err){
-              res.redirect('/wishes?valid='+ newUser._id)
-            }
-            else{
+        if (foundItem.length === 0) {
+          newUser.save(function(err) {
+            if (!err) {
+              res.redirect('/wishes?valid=' + newUser._id)
+            } else {
               console.log(err);
             }
           });
-        }
-        else{
-           res.status(404).send("The specified email is already registered please enter different one!");
+        } else {
+          res.status(404).send("The specified email is already registered please enter different one!");
         }
       }
     });
 
-});
+  });
 
 });
 
@@ -148,16 +153,17 @@ app.post("/register",function(req,res){
 
 
 //To send html and css to the initail screen.
-app.get("/addNotice",function(req,res){
+app.get("/addNotice", function(req, res) {
 
   imgModel.find({}, (err, items) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.render('index.ejs', { items: items });
-        }
-    });
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index.ejs', {
+        items: items
+      });
+    }
+  });
 });
 
 
@@ -167,78 +173,132 @@ app.post('/addNotice', upload.single('image'), (req, res, next) => {
 
 
 
-    var obj = {
-        name: req.body.name,
-        desc: req.body.desc,
-        dept:req.body.department,
-        date:req.body.dat,
-        img: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-            contentType: 'image/png'
-        }
+  var obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    dept: req.body.department,
+    date: req.body.dat,
+    img: {
+      data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+      contentType: 'image/png'
     }
-    imgModel.create(obj, (err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // item.save();
-            res.redirect('/addNotice');
-        }
-    });
+  }
+  imgModel.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect('/addNotice');
+    }
+  });
 });
 
 
-app.post("/delete",function(req,res){
-  imgModel.findByIdAndRemove(req.body.cb,function(err){
-    if(err){
+app.get("/delete/:ide/:dept", function(req, res) {
+  console.log(req.params.ide)
+  console.log(req.params.dept)
+  imgModel.findByIdAndRemove(req.params.ide, function(err) {
+    if (err) {
       console.log(err);
-    }
-    else{
+    } else {
       console.log("successfully deleted!");
       //Code below is to retrive the images of that department after deleted
-      res.redirect("/notice/"+req.body.selecteddept)
+      res.redirect("/notice/" + req.params.dept)
     }
   });
 });
 
-app.get("/notice",function(req,res){
-  res.render("depar.ejs",{ items: [] })
+
+
+app.get("/update/:ide", function(req, res) {
+  //console.log(req.params.ide)
+  imgModel.findById(req.params.ide, function(err, document) {
+    if (err) {
+      console.log(err);
+    } else {
+      //
+      res.render("updatepage", {
+        data: document
+      })
+    }
+  });
+});
+
+
+///Below code is to show updated notice.
+app.post('/update', upload.single('image'), (req, res, next) => {
+
+  imgModel.findByIdAndUpdate(req.body.ide, {
+    name: req.body.name,
+    desc: req.body.desc,
+    dept: req.body.department,
+    date: req.body.dat,
+    img: {
+      data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+      contentType: 'image/png'
+    }
+  }, function(err, document) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("successfully updated!");
+      //console.log(document)
+      res.redirect("/notice/" + req.body.department)
+    }
+  });
+
+
+});
+
+
+
+
+
+app.get("/notice", function(req, res) {
+  res.render("depar.ejs", {
+    items: []
+  })
 })
 
-app.post("/notice/specdept",function(req,res){
+app.post("/notice/specdept", function(req, res) {
   //console.log(req.body.dep)
-  imgModel.find({dept:req.body.dep},(err,items)=>{
+  imgModel.find({
+    dept: req.body.dep
+  }, (err, items) => {
     if (err) {
-        console.log(err);
-    }
-    else {
-        //Below code is to retrive unique dates
-       imgModel.distinct('date',{dept:req.body.dep},(err,ite)=>{
-         if(err){
-           console.log(err);
-         }
-         else{
+      console.log(err);
+    } else {
+      //Below code is to retrive unique dates
+      imgModel.distinct('date', {
+        dept: req.body.dep
+      }, (err, ite) => {
+        if (err) {
+          console.log(err);
+        } else {
 
-           //console.log(ite)
-           res.render('specificdept.ejs', { items: items ,dates:ite});
+          //console.log(ite)
+          res.render('specificdept.ejs', {
+            items: items,
+            dates: ite
+          });
 
-         }
-       });
+        }
+      });
 
     }
   });
 });
 
-app.get("/wishes",function(req,res){
+app.get("/wishes", function(req, res) {
   var passedVariable = req.query.valid;
   //console.log(passedVariable)
 
   User.findById(passedVariable, function(err, item) {
-    if(item!=null){
-      res.render("admin.ejs",{item:item});
-    }
-    else{
+    if (item != null) {
+      res.render("admin.ejs", {
+        item: item
+      });
+    } else {
       console.log("No such User")
     }
   });
@@ -246,48 +306,55 @@ app.get("/wishes",function(req,res){
 
 });
 
-app.post("/wishes",function(req,res){
+app.post("/wishes", function(req, res) {
   // console.log(req.body.wish)
-  if (req.body.wish == "shownotice"){
+  if (req.body.wish == "shownotice") {
     res.redirect("/notice")
-  }
-  else{
+  } else {
     res.redirect("/addNotice")
   }
 })
 
 //Code below to add recieve questions posted.
-app.post("/specdept/addQues",function(req,res){
+app.post("/specdept/addQues", function(req, res) {
   console.log(req.body.idofnot)
 
-  imgModel.findByIdAndUpdate(req.body.idofnot, { "$push": { "ques":req.body.desc } },
-                           function (err, docs) {
-   if (err){
-       console.log(err)
-   }
-   else{
-       console.log("Updated User : ");
-       //Code below is to retrive the images of that department after adding the question.
-       res.redirect("/notice/"+req.body.selecteddept)
-   }
-});
+  imgModel.findByIdAndUpdate(req.body.idofnot, {
+      "$push": {
+        "ques": req.body.desc
+      }
+    },
+    function(err, docs) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("Updated User : ");
+        //Code below is to retrive the images of that department after adding the question.
+        res.redirect("/notice/" + req.body.selecteddept)
+      }
+    });
 
 });
 
-app.get("/notice/:specdept",function(req,res){
-  imgModel.find({dept:req.params.specdept},(err,items)=>{
+app.get("/notice/:specdept", function(req, res) {
+  imgModel.find({
+    dept: req.params.specdept
+  }, (err, items) => {
     if (err) {
-        console.log(err);
-    }
-    else {
-      imgModel.distinct('date',{dept:req.params.specdept},(err,ite)=>{
-        if(err){
+      console.log(err);
+    } else {
+      imgModel.distinct('date', {
+        dept: req.params.specdept
+      }, (err, ite) => {
+        if (err) {
           console.log(err);
-        }
-        else{
+        } else {
 
           //console.log(ite)
-          res.render('specificdept.ejs', { items: items ,dates:ite});
+          res.render('specificdept.ejs', {
+            items: items,
+            dates: ite
+          });
 
         }
       });
@@ -297,26 +364,32 @@ app.get("/notice/:specdept",function(req,res){
 
 //Code below to add recieve answers posted.
 
-app.post("/specdept/addAns",function(req,res){
+app.post("/specdept/addAns", function(req, res) {
   console.log(req.body.idofnot)
 
-  imgModel.findByIdAndUpdate(req.body.idofnot, { "$push": { "answ":req.body.desc } },{ "new": true, "upsert": true },
-                           function (err, docs) {
-   if (err){
-       console.log(err)
-   }
-   else{
-       console.log("Updated User : ");
-       //Code below is to retrive the images of that department after adding the answer.
-       res.redirect("/notice/"+req.body.selecteddept)
-   }
-});
+  imgModel.findByIdAndUpdate(req.body.idofnot, {
+      "$push": {
+        "answ": req.body.desc
+      }
+    }, {
+      "new": true,
+      "upsert": true
+    },
+    function(err, docs) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("Updated User : ");
+        //Code below is to retrive the images of that department after adding the answer.
+        res.redirect("/notice/" + req.body.selecteddept)
+      }
+    });
 
 });
 
 
 
 //Calling the server
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Server is running in port 3000")
 })
