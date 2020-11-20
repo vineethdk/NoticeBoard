c43323933a758b7a48f47074eb3624b1da0e4d2f
vineethdk <vineethdk31@gmail.com>
2020-11-20 17:15:46 +0530

    Changed file system to Gridfs

--- app.js
+++ app.js
@@ -10,9 +10,48 @@ const mongoose = require("mongoose");
 var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
+// testing
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+//till here
 
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+
+// Related to gridfs from here.
+const conn = mongoose.createConnection('mongodb://localhost:27017/imaDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: 'mongodb://localhost:27017/imaDB',
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+        const filename = file.originalname;
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+    });
+  }
+});
+const upload = multer({ storage });
+
+//Till here
+
+
 
 //Connecting to real Database in mongoose website.
 //mongoose.connect('mongodb+srv://admin-vineeth:Test123@cluster0.rbcaz.mongodb.net/<dbname>', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -23,6 +62,9 @@ mongoose.connect('mongodb://localhost:27017/imaDB', {
 });
 
 const app = express();
+
+app.use(methodOverride('_method'));
+
 app.use(bodyParser.urlencoded({
   extended: true
 })); //To grab the form that is entered by user.
@@ -36,28 +78,15 @@ var imageSchema = new mongoose.Schema({
   dept: String,
   date: Date,
   img: {
-    data: Buffer,
-    contentType: String
+    data:[],
+    type:[],
+    ide:[]
   },
   ques: [],
   answ: []
 });
 
 const imgModel = mongoose.model("Image", imageSchema);
-
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-});
-
-
-var upload = multer({
-  storage: storage
-});
 
 
 
@@ -160,7 +189,7 @@ app.get("/addNotice", function(req, res) {
       console.log(err);
     } else {
       res.render('index.ejs', {
-        items: items
+        files: items
       });
     }
   });
@@ -169,8 +198,25 @@ app.get("/addNotice", function(req, res) {
 
 //First posts it into the location spcified in action in html like "/" and
 //and server picks it up that is in the form and posts to the screen after user hits submit butto or something.
-app.post('/addNotice', upload.single('image'), (req, res, next) => {
+app.post('/addNotice', upload.array('myfile',4), (req, res, next) => {
 
+
+  var arr = []
+  var arr1=[]
+  var arr2=[]
+
+  req.files.forEach(function(file){
+     arr.push(file.filename)
+     arr2.push(file.id)
+     if (
+       file.contentType === 'image/jpeg' ||
+       file.contentType === 'image/png'
+     ) {
+       arr1.push(true)
+     } else {
+       arr1.push(false)
+     }
+   });
 
 
   var obj = {
@@ -179,9 +225,10 @@ app.post('/addNotice', upload.single('image'), (req, res, next) => {
     dept: req.body.department,
     date: req.body.dat,
     img: {
-      data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-      contentType: 'image/png'
-    }
+           data:arr,
+           type:arr1,
+           ide:arr2
+         }
   }
   console.log(imgModel)
   imgModel.create(obj, (err, item) => {
@@ -196,13 +243,34 @@ app.post('/addNotice', upload.single('image'), (req, res, next) => {
 
 
 app.get("/delete/:ide/:dept", function(req, res) {
-  console.log(req.params.ide)
-  console.log(req.params.dept)
+
+ var ar = []
+
+  imgModel.findById(req.params.ide, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(result);
+      ar = result.img[0].ide.splice(0)
+    }
+  });
+
+  // console.log(req.params.ide)
+  // console.log(req.params.dept)
+
   imgModel.findByIdAndRemove(req.params.ide, function(err) {
     if (err) {
       console.log(err);
     } else {
       console.log("successfully deleted!");
+      ar.forEach(function(each){
+        gfs.remove({ _id: each, root: 'uploads' }, (err, gridStore) => {
+          if (err) {
+            console.log(arr)
+            return res.status(404).json({ err: err });
+          }
+      });
+    });
       //Code below is to retrive the images of that department after deleted
       res.redirect("/notice/" + req.params.dept)
     }
@@ -257,7 +325,7 @@ app.post('/update', upload.single('image'), (req, res, next) => {
 
 app.get("/notice", function(req, res) {
   res.render("depar.ejs", {
-    items: []
+    files: []
   })
 })
 
@@ -279,7 +347,7 @@ app.post("/notice/specdept", function(req, res) {
 
           //console.log(ite)
           res.render('specificdept.ejs', {
-            items: items,
+            files: items,
             dates: ite
           });
 
@@ -353,7 +421,7 @@ app.get("/notice/:specdept", function(req, res) {
 
           //console.log(ite)
           res.render('specificdept.ejs', {
-            items: items,
+            files: items,
             dates: ite
           });
 
@@ -386,6 +454,124 @@ app.post("/specdept/addAns", function(req, res) {
       }
     });
 
+});
+
+
+//To display gridfs images and files..
+
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+    // File exists
+    const readstream = gfs.createReadStream(file.filename);
+    return readstream.pipe(res);
+  });
+});
+
+
+
+app.post('/files', (req, res) => {
+  // console.log(req.body)
+  // console.log(req.body.idoffile);
+  // console.log(req.body.ind)
+  var arr = []
+  var arr1=[]
+  var arr2=[]
+
+
+
+// To split the incoming string to array.
+arr= req.body.dataarr.split(",");
+
+var removed = arr.splice(req.body.ind,1);
+
+// To split the incoming string to array and covert to boolean.
+
+arr1= req.body.typearr.split(",");
+
+removed = arr1.splice(req.body.ind,1);
+
+
+var temp=[]
+arr1.forEach(function(a){
+  if (a=='false'){
+    temp.push(false)
+  }
+  else{
+    temp.push(true)
+  }
+});
+
+arr1=temp.splice(0)
+
+// To split the incoming string to array and covert to ObjectId.
+
+arr2= req.body.idearr.split(",");
+
+removed = arr2.splice(req.body.ind,1);
+
+var temp=[]
+arr2.forEach(function(a){
+  temp.push(mongoose.Types.ObjectId(a))
+});
+
+arr2 = temp.splice(0)
+
+//Below code is to assign the factored arrays to the database.
+
+
+imgModel.findByIdAndUpdate(req.body.idoffile, { img : {data: arr , type: arr1 , ide: arr2}},
+                          function (err, docs) {
+  if (err){
+      //console.log("olu")
+      console.log(err)
+  }
+  else{
+      console.log("Successfully updated");
+  }
+});
+
+
+  gfs.remove({ _id: req.body.idea, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      console.log(arr)
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect("/notice/" + req.body.dept)
+  });
+});
+
+
+
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+  //console.log("iamhere")
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
 });
 
 
